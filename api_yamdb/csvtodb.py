3d85fc265d3dd9ad6c482f3a9df8a500a9757b44
unknown <coofi2012@gmail.com>
2022-06-24 21:32:32 +0300

    Ready to review #2

--- api_yamdb/csvtodb.py
+++ api_yamdb/csvtodb.py
@@ -4,14 +4,20 @@ import sqlite3
 
 conn = sqlite3.connect(r'D:\Dev\api_yamdb\api_yamdb\db.sqlite3')
 cur = conn.cursor()
-with open(r"D:\Dev\api_yamdb\api_yamdb\static\data\titles.csv", 'r', encoding='UTF-8') as csv_file:
+with open(
+    r"D:\Dev\api_yamdb\api_yamdb\static\data\titles.csv",
+    'r',
+    encoding='UTF-8'
+) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     header = next(csv_reader)
     for row in csv_reader:
-        cur.execute("INSERT INTO reviews_titles(name,year,category_id) VALUES (?,?,?)", (
-            str(row[1]),
-            int(row[2]),
-            int(row[3]),
+        cur.execute(
+            "INSERT INTO reviews_titles(name,year,category_id) VALUES (?,?,?)",
+            (
+                str(row[1]),
+                int(row[2]),
+                int(row[3]),
+            )
         )
-    )
     conn.commit()
